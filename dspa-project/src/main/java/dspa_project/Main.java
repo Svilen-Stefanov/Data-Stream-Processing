099daac1a6c0dc7435d1884e25d9cbf5f2443660
Dimy93 <dimitadi@student.ethz.ch>
2019-05-25 13:18:37 +0100

    Added task selection

--- dspa-project/src/main/java/dspa_project/Main.java
+++ dspa-project/src/main/java/dspa_project/Main.java
@@ -19,34 +19,60 @@ import java.util.*;
 public class Main {
 
 	private static KafkaCreator kafkaCreator = null;
+	private final static boolean[][] ALL_TASKS = new boolean[][] {
+			{ true, true, true }, //Task1.1, Task1.2, Task1.3
+			{ true, true }, // Task2_Static, Task2_Dynamic
+			{ true } }; //Task3
+
+	private final static boolean[][] No_TASKS = new boolean[][] {
+			{ false, false, false }, //Task1.1, Task1.2, Task1.3
+			{ false, false }, // Task2_Static, Task2_Dynamic
+			{ false } }; //Task3
+
 
 	public static void main(String[] args) throws Exception {
 
 		if ( kafkaCreator == null )
 			kafkaCreator = new KafkaCreator();
 
-		parseArguments(args);
+		boolean[][] tasks = parseArguments(args);
 		DataLoader dataLoader = new DataLoader();
 		dataLoader.parseStaticData();
 
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-		EventCountStream task1_1 = new EventCountStream(env, "Task1_1", Time.minutes(30), Time.hours(12), false);
-		task1_1.writeToFile( ConfigLoader.getTask1_1_path() );
-		EventCountStream task1_2 = new EventCountStream(env,"Task1_2", Time.minutes(30), Time.hours(12), true);
-		task1_2.writeToFile( ConfigLoader.getTask1_2_path() );
-		UniquePeopleCountStream task1_3 = new UniquePeopleCountStream(env,"Task1_3", Time.hours(1), Time.hours(12), false);
-		task1_3.writeToFile( ConfigLoader.getTask1_3_path() );
-		
-		Task2_Static task2_static = new Task2_Static(env);
-		Task2_Dynamic task2_dynamic = new Task2_Dynamic(env);
-		Task3 task3 = new Task3(env);
-
+		if ( tasks[0][0] ) {
+			System.out.println("Executing Task 1.1");
+			EventCountStream task1_1 = new EventCountStream(env, "Task1_1", Time.minutes(30), Time.hours(12), false);
+			task1_1.writeToFile(ConfigLoader.getTask1_1_path());
+		}
+		if (tasks[0][1]) {
+			System.out.println("Executing Task 1.2");
+			EventCountStream task1_2 = new EventCountStream(env, "Task1_2", Time.minutes(30), Time.hours(12), true);
+			task1_2.writeToFile(ConfigLoader.getTask1_2_path());
+		}
+		if ( tasks[0][2] ) {
+			System.out.println("Executing Task 1.3");
+			UniquePeopleCountStream task1_3 = new UniquePeopleCountStream(env, "Task1_3", Time.hours(1), Time.hours(12), false);
+			task1_3.writeToFile(ConfigLoader.getTask1_3_path());
+		}
+		if ( tasks[1][0] ) {
+			System.out.println("Executing Task 2 static");
+			Task2_Static task2_static = new Task2_Static(env);
+		}
+		if ( tasks[1][1] ) {
+			System.out.println("Executing Task 2 dynamic");
+			Task2_Dynamic task2_dynamic = new Task2_Dynamic(env);
+		}
+		if( tasks[2][0] ) {
+			System.out.println("Executing Task 3");
+			Task3 task3 = new Task3(env);
+		}
 		env.execute("Flink Streaming Java API Skeleton");
 	}
 
-	private static void parseArguments(String[] args) throws IOException, ClassNotFoundException {
+	private static boolean[][] parseArguments(String[] args) throws IOException, ClassNotFoundException {
 		Map<String, List<String>> params = new HashMap<>();
 
 		List<String> options = null;
@@ -99,5 +125,37 @@ public class Main {
 			}
 			kafkaCreator.startPostStream( count );
 		}
+
+		if (params.get("task") != null){
+			boolean[][] tasks = No_TASKS;
+			for ( String task : params.get("task") ) {
+				switch(task)
+				{
+					case "1.1":
+						tasks[0][0] = true;
+						break;
+					case "1.2":
+						tasks[0][1] = true;
+						break;
+					case "1.3":
+						tasks[0][2] = true;
+						break;
+					case "2.S":
+						tasks[1][0] = true;
+						break;
+					case "2.D":
+						tasks[1][1] = true;
+						break;
+					case "3":
+						tasks[2][0] = true;
+						break;
+					default:
+						System.err.println("Wrong task. Must be one of \"1.1\",  \"1.1\", \"1.3\", \"2.S\", \"2.D\", \"3\".");
+						System.exit(1);
+				}
+			}
+			return tasks;
+		}
+		return ALL_TASKS;
 	}
 }
