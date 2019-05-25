c9feb5b0436645ff60711134dea32080f02c6637
Dimy93 <dimitadi@student.ethz.ch>
2019-05-25 12:36:20 +0100

    Added test for 1.3 . Fixed a bug in 1.3

--- dspa-project/src/main/java/dspa_project/tasks/task1/UniquePeopleStream.java
+++ dspa-project/src/main/java/dspa_project/tasks/task1/UniquePeopleStream.java
@@ -1,12 +1,11 @@
 package dspa_project.tasks.task1;
 
 import dspa_project.model.EventInterface;
-import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.RichAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
@@ -22,63 +21,25 @@ public class UniquePeopleStream {
     private final Time tumblingSize;
     private final Time activeWindow;
 
-    private static class UniquePeopleAggregate implements AggregateFunction< UniquePeople, UniquePeoplePostCollection, UniquePeoplePostCollection > {
-
+    private static class UniquePeopleAggregate extends RichAllWindowFunction<UniquePeople, Tuple2<Date,UniquePeoplePostCollection>, TimeWindow> {
         @Override
-        public UniquePeoplePostCollection createAccumulator() {
-            return new UniquePeoplePostCollection();
-        }
-
-        @Override
-        public UniquePeoplePostCollection merge(UniquePeoplePostCollection lhs, UniquePeoplePostCollection rhs) {
-            for ( Long key : rhs.keySet() ) {
-                if ( lhs.containsKey(key) ) {
-                    HashMap<Long, HashSet<String>> ppl = lhs.get( key );
-                    for ( Map.Entry<Long,HashSet<String>> person : rhs.get( key ).entrySet() ) {
+        public void apply(TimeWindow timeWindow, Iterable<UniquePeople> iterable, Collector< Tuple2<Date,UniquePeoplePostCollection>> collector) throws Exception {
+            //System.out.println(getRuntimeContext().getIndexOfThisSubtask() + ">Process2 " + new Date(timeWindow.getEnd()) + iterable);
+            long time = timeWindow.getEnd();
+            UniquePeoplePostCollection out = new UniquePeoplePostCollection();
+            for ( UniquePeople post : iterable) {
+                if ( !out.containsKey( post.f0 ) ){
+                    out.put( post.f0, post.f1 );
+                } else {
+                    HashMap<Long, HashSet<String>> ppl = out.get( post.f0 );
+                    for ( Map.Entry<Long,HashSet<String>> person : post.f1.entrySet() ) {
                         if ( ppl.containsKey( person.getKey() ) )  {
                             ppl.get( person.getKey() ).addAll( person.getValue() );
                         } else {
                             ppl.put( person.getKey(), person.getValue() );
                         }
                     }
-                } else {
-                    lhs.put( key, rhs.get(key) );
                 }
-            }
-
-            return lhs;
-        }
-
-        @Override
-        public UniquePeoplePostCollection add( UniquePeople el, UniquePeoplePostCollection acc ) {
-            if ( !acc.containsKey( el.f0 ) ){
-                acc.put( el.f0, el.f1 );
-            } else {
-                HashMap<Long, HashSet<String>> ppl = acc.get( el.f0 );
-                for ( Map.Entry<Long,HashSet<String>> person : el.f1.entrySet() ) {
-                    if ( ppl.containsKey( person.getKey() ) )  {
-                        ppl.get( person.getKey() ).addAll( person.getValue() );
-                    } else {
-                        ppl.put( person.getKey(), person.getValue() );
-                    }
-                }
-            }
-            return acc;
-        }
-        @Override
-        public UniquePeoplePostCollection getResult(UniquePeoplePostCollection acc) {
-            return acc;
-        }
-    }
-
-    private static class GetTimestampUniquePeople extends ProcessAllWindowFunction<UniquePeoplePostCollection, Tuple2<Date,UniquePeoplePostCollection>, TimeWindow> {
-
-        @Override
-        public void process(Context context, Iterable<UniquePeoplePostCollection> iterable, Collector<Tuple2<Date, UniquePeoplePostCollection>> collector) {
-            long time = context.window().getEnd();
-            UniquePeoplePostCollection out = new UniquePeoplePostCollection();
-            for ( UniquePeoplePostCollection posts : iterable) {
-                out.putAll(posts);
             }
             collector.collect(new Tuple2<>(new Date(time),out));
         }
@@ -98,7 +59,7 @@ public class UniquePeopleStream {
     }
 
     private DataStream<Tuple2<Date,UniquePeoplePostCollection>> createStream( DataStream< EventsCollection > all_stream ){
-        DataStream<Tuple2<Date,UniquePeoplePostCollection>> stream = all_stream.map(new MapFunction< EventsCollection, UniquePeople >() {
+        DataStream< Tuple2<Date,UniquePeoplePostCollection> > stream = all_stream.map(new MapFunction< EventsCollection, UniquePeople >() {
             @Override
             public UniquePeople map(EventsCollection in) {
                 HashMap<Long, HashSet<String>> ids =  new HashMap<>();
@@ -111,7 +72,8 @@ public class UniquePeopleStream {
                 }
                 return new UniquePeople( in.get(0).getPostId(), ids );
             }
-        }).windowAll( SlidingEventTimeWindows.of( this.activeWindow, this.tumblingSize ) ).aggregate( new UniquePeopleAggregate(), new GetTimestampUniquePeople() );
+        }).windowAll( SlidingEventTimeWindows.of( this.activeWindow, this.tumblingSize ) ).apply(new UniquePeopleAggregate());
+
         return stream;
     }
 
